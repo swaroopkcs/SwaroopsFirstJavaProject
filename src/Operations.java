828e3f6c89f5688a59135cc8c9f0a12f16a38709
Swaroop <swaroopkcs@gmail.com>
2021-12-16 21:48:59 -0500

    operations assignment

---
+++ src/Operations.java
@@ -0,0 +1,20 @@
+
+public class Operations {
+
+	public static void main(String[] args) {
+		int num1, num2;
+		num1 = 15;
+		num2 = 45;
+		int sum = num1+num2;
+		int difference = num1-num2;
+		int product = num1*num2;
+		System.out.println("The sum of the numbers 15 and 45 is:  "+sum);
+		System.out.println("");
+		System.out.println("The difference of the numbers 15 and 45 is:  "+difference);
+		System.out.println("");
+		System.out.println("The product of the numbers 15 and 45 is:   "+product);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		int length, breadth;
+		int perimeter, area;
+		length = 10;
+		breadth = 5;
+		perimeter = 2*(length+breadth);
+		area = length*breadth;
+		System.out.println("The perimeter of a rectangle of length 10 and breadth 5 is:  "+perimeter);
+		System.out.println("");
+		System.out.println("The area of a rectangle of length 10 and breadth 5 is:   "+area);				
+		// TODO Auto-generated method stub
+
+	}
+
+}
